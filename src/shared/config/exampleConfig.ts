e55b8d63613bc0f8bb08d3665441acfc3492b908
Alexander Lamdan <alexanderlamdan2@zohomail.com>
2025-06-16 16:23:14 +0300

    Adding exampleConfig.ts

---
+++ src/shared/config/exampleConfig.ts
@@ -0,0 +1,10 @@
+export const exampleConfig = {
+
+	bot_token:"123456789:abcderetdferer354dfjdf",
+	url:"https://your-domain.com",
+	url_path:"/your/url_path",
+	mode:"polling or webhook",
+	admin_chat_id:1234567890,
+	port: 'portnumber'
+
+}
